be3ad5eb7fe7f31ef0426df083f8092d00b884ae
samandar-dev <samandarbohodirov01@gmail.com>
2022-04-05 13:54:49 +0500

    add

--- js/main.js
+++ js/main.js
@@ -1,26 +1,19 @@
 const secInner = document.querySelector('.sec__inner');
-
 let arr = []
-let box = []
 
-object.forEach((obj, index) => {
-  // let secBox = document.createElement('div');
-  // secBox.className = "sec__box"
+object.forEach((obj) => {
   let li = document.createElement('li');
   li.className = "item"
   li.innerHTML = `
   <div class="sec__box">
-    <button class="sec__box-left-btn btn-hov">
+    <button class="sec__box-left-btn btn-hov btns-l-r">
       <i class='bx bx-chevron-left'></i>
     </button>
     <div class="img-box">
       <img class="img" src="${obj.mediya[0]}" alt="img">
     </div>
     <div class="sec__header head">
-      <div class="head__top-anim col-12 row-cols-2 d-flex align-items-center">
-        <div class="head__anim-box">
-          <span class="head__anim-span anim"></span>
-        </div>
+      <div class="head__top-anim col-12 row-cols-1 d-flex align-items-center">
         <div class="head__anim-box">
           <span class="head__anim-span"></span>
         </div>
@@ -52,7 +45,7 @@ object.forEach((obj, index) => {
         </button>
       </form>
     </div>
-    <button class="sec__box-right-btn btn-hov">
+    <button class="sec__box-right-btn btn-hov btns-l-r">
       <i class='bx bx-chevron-right'></i>
     </button>
   </div>`;
@@ -61,149 +54,348 @@ object.forEach((obj, index) => {
   secInner.appendChild(li)
 })
 
+let timeout;
+let count = 1;
+let n = 1;
+
+function myFunction() {
+  timeout = setTimeout(alertFunc, 3000);
+}
+
 let sBox = document.querySelectorAll('.sec__box');
+let animSpan = document.querySelectorAll('.head__anim-span');
+let secHeader = document.querySelectorAll('.sec__header');
 
-sBox[0].classList.add('act');
-secInner.style.transform = 'translateX(600px)';
+myFunction();
 
-arr[0].addEventListener('click', () => {
-  sBox[0].classList.add('act');
-  sBox[1].classList.remove('act');
-  sBox[2].classList.remove('act');
-  sBox[3].classList.remove('act');
-  sBox[4].classList.remove('act');
-  sBox[5].classList.remove('act');
-  sBox[6].classList.remove('act');
-  sBox[7].classList.remove('act');
-  sBox[8].classList.remove('act');
-  sBox[9].classList.remove('act');
+sBox[0].classList.add("act");
+animSpan[0].classList.add('anim');
+secInner.style.transform = 'translateX(630px)';
 
-  secInner.style.transform = 'translateX(600px)';
-})
+sBox.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.add("click");
+    checkIt();
+  });
+});
 
-arr[1].addEventListener('click', () => {
-  sBox[0].classList.remove('act');
-  sBox[1].classList.add('act');
-  sBox[2].classList.remove('act');
-  sBox[3].classList.remove('act');
-  sBox[4].classList.remove('act');
-  sBox[5].classList.remove('act');
-  sBox[6].classList.remove('act');
-  sBox[7].classList.remove('act');
-  sBox[8].classList.remove('act');
-  sBox[9].classList.remove('act');
-  secInner.style.transform = 'translateX(100px)';
+function checkIt() {
+  sBox.forEach((item, inx) => {
+    if (item.classList.contains("click")) {
+      item.classList.add("act");
+      item.classList.remove("click");
+      listFunc(inx)
+    } else {
+      item.classList.remove("act")
+    }
+  });
+}
 
-})
+function listFunc(lis) {
+  count = lis;
+  alertFunc();
 
-arr[2].addEventListener('click', () => {
-  sBox[0].classList.remove('act');
-  sBox[1].classList.remove('act');
-  sBox[2].classList.add('act');
-  sBox[3].classList.remove('act');
-  sBox[4].classList.remove('act');
-  sBox[5].classList.remove('act');
-  sBox[6].classList.remove('act');
-  sBox[7].classList.remove('act');
-  sBox[8].classList.remove('act');
-  sBox[9].classList.remove('act');
-  secInner.style.transform = 'translateX(-400px)';
-})
+  if (lis == 0) {
+    secInner.style.transform = 'translateX(630px)';
+  }
+  else if (lis == 1) {
+    secInner.style.transform = 'translateX(260px)';
+  }
+  else if (lis == 2) {
+    secInner.style.transform = 'translateX(-80px)';
+  }
+  else if (lis == 3) {
+    secInner.style.transform = 'translateX(-440px)';
+  }
+  else if (lis == 4) {
+    secInner.style.transform = 'translateX(-800px)';
+  }
+  else if (lis == 5) {
+    secInner.style.transform = 'translateX(-1170px)';
+  }
+  else if (lis == 6) {
+    secInner.style.transform = 'translateX(-1490px)';
+  }
+  else if (lis == 7) {
+    secInner.style.transform = 'translateX(-1850px)';
+  }
+  else if (lis == 8) {
+    secInner.style.transform = 'translateX(-2070px)';
+  }
+  else if (lis == 9) {
+    secInner.style.transform = 'translateX(-2410px)';
+  }
+}
 
-arr[3].addEventListener('click', () => {
-  sBox[0].classList.remove('act');
-  sBox[1].classList.remove('act');
-  sBox[2].classList.remove('act');
-  sBox[3].classList.add('act');
-  sBox[4].classList.remove('act');
-  sBox[5].classList.remove('act');
-  sBox[6].classList.remove('act');
-  sBox[7].classList.remove('act');
-  sBox[8].classList.remove('act');
-  sBox[9].classList.remove('act');
-  secInner.style.transform = 'translateX(-950px)';
-})
+function alertFunc() {
+  if (count == 0) {
+    sBox[0].classList.add('act');
+    sBox[1].classList.remove('act');
+    sBox[2].classList.remove('act');
+    sBox[3].classList.remove('act');
+    sBox[4].classList.remove('act');
+    sBox[5].classList.remove('act');
+    sBox[6].classList.remove('act');
+    sBox[7].classList.remove('act');
+    sBox[8].classList.remove('act');
+    sBox[9].classList.remove('act');
 
-arr[4].addEventListener('click', () => {
-  sBox[0].classList.remove('act');
-  sBox[1].classList.remove('act');
-  sBox[2].classList.remove('act');
-  sBox[3].classList.remove('act');
-  sBox[4].classList.add('act');
-  sBox[5].classList.remove('act');
-  sBox[6].classList.remove('act');
-  sBox[7].classList.remove('act');
-  sBox[8].classList.remove('act');
-  sBox[9].classList.remove('act');
-  secInner.style.transform = 'translateX(-1450px)';
-})
+    animSpan[0].classList.add('anim');
+    animSpan[1].classList.remove('anim');
+    animSpan[2].classList.remove('anim');
+    animSpan[3].classList.remove('anim');
+    animSpan[4].classList.remove('anim');
+    animSpan[5].classList.remove('anim');
+    animSpan[6].classList.remove('anim');
+    animSpan[7].classList.remove('anim');
+    animSpan[8].classList.remove('anim');
+    animSpan[9].classList.remove('anim');
 
-arr[5].addEventListener('click', () => {
-  sBox[0].classList.remove('act');
-  sBox[1].classList.remove('act');
-  sBox[2].classList.remove('act');
-  sBox[3].classList.remove('act');
-  sBox[4].classList.remove('act');
-  sBox[5].classList.add('act');
-  sBox[6].classList.remove('act');
-  sBox[7].classList.remove('act');
-  sBox[8].classList.remove('act');
-  sBox[9].classList.remove('act');
-  secInner.style.transform = 'translateX(-2000px)';
-})
+    secInner.style.transform = 'translateX(630px)';
+    count++
+    myFunction()
+  }
+  else if (count == 1) {
+    sBox[0].classList.remove('act');
+    sBox[1].classList.add('act');
+    sBox[2].classList.remove('act');
+    sBox[3].classList.remove('act');
+    sBox[4].classList.remove('act');
+    sBox[5].classList.remove('act');
+    sBox[6].classList.remove('act');
+    sBox[7].classList.remove('act');
+    sBox[8].classList.remove('act');
+    sBox[9].classList.remove('act');
 
-arr[6].addEventListener('click', () => {
-  sBox[0].classList.remove('act');
-  sBox[1].classList.remove('act');
-  sBox[2].classList.remove('act');
-  sBox[3].classList.remove('act');
-  sBox[4].classList.remove('act');
-  sBox[5].classList.remove('act');
-  sBox[6].classList.add('act');
-  sBox[7].classList.remove('act');
-  sBox[8].classList.remove('act');
-  sBox[9].classList.remove('act');
-  secInner.style.transform = 'translateX(-2550px)';
-})
+    animSpan[0].classList.remove('anim');
+    animSpan[1].classList.add('anim');
+    animSpan[2].classList.remove('anim');
+    animSpan[3].classList.remove('anim');
+    animSpan[4].classList.remove('anim');
+    animSpan[5].classList.remove('anim');
+    animSpan[6].classList.remove('anim');
+    animSpan[7].classList.remove('anim');
+    animSpan[8].classList.remove('anim');
+    animSpan[9].classList.remove('anim');
 
-arr[7].addEventListener('click', () => {
-  sBox[0].classList.remove('act');
-  sBox[1].classList.remove('act');
-  sBox[2].classList.remove('act');
-  sBox[3].classList.remove('act');
-  sBox[4].classList.remove('act');
-  sBox[5].classList.remove('act');
-  sBox[6].classList.remove('act');
-  sBox[7].classList.add('act');
-  sBox[8].classList.remove('act');
-  sBox[9].classList.remove('act');
-  secInner.style.transform = 'translateX(-3100px)';
-})
+    secInner.style.transform = 'translateX(300px)';
+    count++
+    myFunction()
+  }
+  else if (count == 2) {
+    sBox[0].classList.remove('act');
+    sBox[1].classList.remove('act');
+    sBox[2].classList.add('act');
+    sBox[3].classList.remove('act');
+    sBox[4].classList.remove('act');
+    sBox[5].classList.remove('act');
+    sBox[6].classList.remove('act');
+    sBox[7].classList.remove('act');
+    sBox[8].classList.remove('act');
+    sBox[9].classList.remove('act');
 
-arr[8].addEventListener('click', () => {
-  sBox[0].classList.remove('act');
-  sBox[1].classList.remove('act');
-  sBox[2].classList.remove('act');
-  sBox[3].classList.remove('act');
-  sBox[4].classList.remove('act');
-  sBox[5].classList.remove('act');
-  sBox[6].classList.remove('act');
-  sBox[7].classList.remove('act');
-  sBox[8].classList.add('act');
-  sBox[9].classList.remove('act');
-  secInner.style.transform = 'translateX(-3600px)';
-})
+    animSpan[0].classList.remove('anim');
+    animSpan[1].classList.remove('anim');
+    animSpan[2].classList.add('anim');
+    animSpan[3].classList.remove('anim');
+    animSpan[4].classList.remove('anim');
+    animSpan[5].classList.remove('anim');
+    animSpan[6].classList.remove('anim');
+    animSpan[7].classList.remove('anim');
+    animSpan[8].classList.remove('anim');
+    animSpan[9].classList.remove('anim');
 
-arr[9].addEventListener('click', () => {
-  sBox[0].classList.remove('act');
-  sBox[1].classList.remove('act');
-  sBox[2].classList.remove('act');
-  sBox[3].classList.remove('act');
-  sBox[4].classList.remove('act');
-  sBox[5].classList.remove('act');
-  sBox[6].classList.remove('act');
-  sBox[7].classList.remove('act');
-  sBox[8].classList.remove('act');
-  sBox[9].classList.add('act');
-  secInner.style.transform = 'translateX(-4000px)';
-})
+    secInner.style.transform = 'translateX(-50px)';
+    count++
+    myFunction()
+  }
+  else if (count == 3) {
+    sBox[0].classList.remove('act');
+    sBox[1].classList.remove('act');
+    sBox[2].classList.remove('act');
+    sBox[3].classList.add('act');
+    sBox[4].classList.remove('act');
+    sBox[5].classList.remove('act');
+    sBox[6].classList.remove('act');
+    sBox[7].classList.remove('act');
+    sBox[8].classList.remove('act');
+    sBox[9].classList.remove('act');
+
+    animSpan[0].classList.remove('anim');
+    animSpan[1].classList.remove('anim');
+    animSpan[2].classList.remove('anim');
+    animSpan[3].classList.add('anim');
+    animSpan[4].classList.remove('anim');
+    animSpan[5].classList.remove('anim');
+    animSpan[6].classList.remove('anim');
+    animSpan[7].classList.remove('anim');
+    animSpan[8].classList.remove('anim');
+    animSpan[9].classList.remove('anim');
+
+    secInner.style.transform = 'translateX(-390px)';
+    count++
+    myFunction()
+  }
+  else if (count == 4) {
+    sBox[0].classList.remove('act');
+    sBox[1].classList.remove('act');
+    sBox[2].classList.remove('act');
+    sBox[3].classList.remove('act');
+    sBox[4].classList.add('act');
+    sBox[5].classList.remove('act');
+    sBox[6].classList.remove('act');
+    sBox[7].classList.remove('act');
+    sBox[8].classList.remove('act');
+    sBox[9].classList.remove('act');
+
+    animSpan[0].classList.remove('anim');
+    animSpan[1].classList.remove('anim');
+    animSpan[2].classList.remove('anim');
+    animSpan[3].classList.remove('anim');
+    animSpan[4].classList.add('anim');
+    animSpan[5].classList.remove('anim');
+    animSpan[6].classList.remove('anim');
+    animSpan[7].classList.remove('anim');
+    animSpan[8].classList.remove('anim');
+    animSpan[9].classList.remove('anim');
+
+    secInner.style.transform = 'translateX(-720px)';
+    count++
+    myFunction()
+  }
+  else if (count == 5) {
+    sBox[0].classList.remove('act');
+    sBox[1].classList.remove('act');
+    sBox[2].classList.remove('act');
+    sBox[3].classList.remove('act');
+    sBox[4].classList.remove('act');
+    sBox[5].classList.add('act');
+    sBox[6].classList.remove('act');
+    sBox[7].classList.remove('act');
+    sBox[8].classList.remove('act');
+    sBox[9].classList.remove('act');
+
+    animSpan[0].classList.remove('anim');
+    animSpan[1].classList.remove('anim');
+    animSpan[2].classList.remove('anim');
+    animSpan[3].classList.remove('anim');
+    animSpan[4].classList.remove('anim');
+    animSpan[5].classList.add('anim');
+    animSpan[6].classList.remove('anim');
+    animSpan[7].classList.remove('anim');
+    animSpan[8].classList.remove('anim');
+    animSpan[9].classList.remove('anim');
+
+    secInner.style.transform = 'translateX(-1050px)';
+    count++
+    myFunction()
+  }
+  else if (count == 6) {
+    sBox[0].classList.remove('act');
+    sBox[1].classList.remove('act');
+    sBox[2].classList.remove('act');
+    sBox[3].classList.remove('act');
+    sBox[4].classList.remove('act');
+    sBox[5].classList.remove('act');
+    sBox[6].classList.add('act');
+    sBox[7].classList.remove('act');
+    sBox[8].classList.remove('act');
+    sBox[9].classList.remove('act');
+
+    animSpan[0].classList.remove('anim');
+    animSpan[1].classList.remove('anim');
+    animSpan[2].classList.remove('anim');
+    animSpan[3].classList.remove('anim');
+    animSpan[4].classList.remove('anim');
+    animSpan[5].classList.remove('anim');
+    animSpan[6].classList.add('anim');
+    animSpan[7].classList.remove('anim');
+    animSpan[8].classList.remove('anim');
+    animSpan[9].classList.remove('anim');
+
+    secInner.style.transform = 'translateX(-1390px)';
+    count++
+    myFunction()
+  }
+  else if (count == 7) {
+    sBox[0].classList.remove('act');
+    sBox[1].classList.remove('act');
+    sBox[2].classList.remove('act');
+    sBox[3].classList.remove('act');
+    sBox[4].classList.remove('act');
+    sBox[5].classList.remove('act');
+    sBox[6].classList.remove('act');
+    sBox[7].classList.add('act');
+    sBox[8].classList.remove('act');
+    sBox[9].classList.remove('act');
+
+    animSpan[0].classList.remove('anim');
+    animSpan[1].classList.remove('anim');
+    animSpan[2].classList.remove('anim');
+    animSpan[3].classList.remove('anim');
+    animSpan[4].classList.remove('anim');
+    animSpan[5].classList.remove('anim');
+    animSpan[6].classList.remove('anim');
+    animSpan[7].classList.add('anim');
+    animSpan[8].classList.remove('anim');
+    animSpan[9].classList.remove('anim');
+
+    secInner.style.transform = 'translateX(-1730px)';
+    count++
+    myFunction()
+  }
+  else if (count == 8) {
+    sBox[0].classList.remove('act');
+    sBox[1].classList.remove('act');
+    sBox[2].classList.remove('act');
+    sBox[3].classList.remove('act');
+    sBox[4].classList.remove('act');
+    sBox[5].classList.remove('act');
+    sBox[6].classList.remove('act');
+    sBox[7].classList.remove('act');
+    sBox[8].classList.add('act');
+    sBox[9].classList.remove('act');
+
+    animSpan[0].classList.remove('anim');
+    animSpan[1].classList.remove('anim');
+    animSpan[2].classList.remove('anim');
+    animSpan[3].classList.remove('anim');
+    animSpan[4].classList.remove('anim');
+    animSpan[5].classList.remove('anim');
+    animSpan[6].classList.remove('anim');
+    animSpan[7].classList.remove('anim');
+    animSpan[8].classList.add('anim');
+    animSpan[9].classList.remove('anim');
+
+    secInner.style.transform = 'translateX(-2070px)';
+    count++
+    myFunction()
+  }
+  else if (count == 9) {
+    sBox[0].classList.remove('act');
+    sBox[1].classList.remove('act');
+    sBox[2].classList.remove('act');
+    sBox[3].classList.remove('act');
+    sBox[4].classList.remove('act');
+    sBox[5].classList.remove('act');
+    sBox[6].classList.remove('act');
+    sBox[7].classList.remove('act');
+    sBox[8].classList.remove('act');
+    sBox[9].classList.add('act');
+
+    animSpan[0].classList.remove('anim');
+    animSpan[1].classList.remove('anim');
+    animSpan[2].classList.remove('anim');
+    animSpan[3].classList.remove('anim');
+    animSpan[4].classList.remove('anim');
+    animSpan[5].classList.remove('anim');
+    animSpan[6].classList.remove('anim');
+    animSpan[7].classList.remove('anim');
+    animSpan[8].classList.remove('anim');
+    animSpan[9].classList.add('anim');
+
+    secInner.style.transform = 'translateX(-2410px)';
+    count = 0
+    myFunction()
+  }
+}
--- js/object.js
+++ js/object.js
@@ -4,34 +4,35 @@ const object = [
     avatar: "images/ava1.png",
     mediya: [
       "images/img3.jpg",
+      "images/img2.jpg",
+      "images/img3.jpg",
     ]
   },
   {
     id: 2,
     avatar: "images/ava2.jpg",
     mediya: [
-      "images/img2.jpg",
-      "images/img3.jpg",
-      "images/img4.jpg",
+      "images/img6.jpg",
+      "images/img5.jpg",
+      "images/img6.jpg",
     ]
   },
   {
     id: 3,
     avatar: "images/ava3.png",
     mediya: [
-      "images/img5.jpg",
       "images/img2.jpg",
-      "images/img3.jpg",
-      "images/img6.jpg",
-      "images/img5.jpg",
+      "images/img4.jpg",
+      "images/img2.jpg",
     ]
   },
   {
     id: 4,
     avatar: "images/ava4.png",
     mediya: [
-      "images/img6.jpg",
-      "images/img3.jpg",
+      "images/img4.jpg",
+      "images/img5.jpg",
+      "images/img4.jpg",
     ]
   },
 
@@ -39,17 +40,18 @@ const object = [
     id: 5,
     avatar: "images/ava5.png",
     mediya: [
-      "images/img1.jpg",
-      "images/img5.jpg",
       "images/img2.jpg",
-      "images/img6.jpg",
+      "images/img1.jpg",
+      "images/img1.jpg",
     ]
   },
   {
     id: 6,
     avatar: "images/ava1.png",
     mediya: [
-      "images/img1.jpg",
+      "images/img4.jpg",
+      "images/img5.jpg",
+      "images/img6.jpg",
     ]
   },
   {
@@ -57,18 +59,17 @@ const object = [
     avatar: "images/ava2.png",
     mediya: [
       "images/img2.jpg",
-      "images/img3.jpg",
       "images/img4.jpg",
+      "images/img5.jpg",
+      "images/img6.jpg",
     ]
   },
   {
     id: 8,
     avatar: "images/ava3.png",
     mediya: [
-      "images/img5.jpg",
       "images/img2.jpg",
-      "images/img3.jpg",
-      "images/img6.jpg",
+      "images/img1.jpg",
       "images/img5.jpg",
     ]
   },
